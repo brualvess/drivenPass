3cf51d93bd7e0c533eac8a3472ca9d9499875752
brualvess <oliveirabrunna2210@gmail.com>
2022-09-06 17:45:14 -0300

    routes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import { json } from 'express';
+import router from './routes.js'
 
 
 
@@ -10,7 +11,7 @@ dotenv.config()
 const app = express()
 app.use(json());
 app.use(cors());
-
+app.use(router)
 
 app.listen(parseInt(process.env.PORT), () => {
     console.log(`Server on port ${process.env.PORT}`)
