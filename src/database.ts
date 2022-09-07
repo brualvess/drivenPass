8cee47398946f1d431b1ab4a0fda737612b7ef4e
brualvess <oliveirabrunna2210@gmail.com>
2022-09-07 17:17:50 -0300

    migrations

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,7 @@
-import dotenv from "dotenv";
-import pg from "pg";
-dotenv.config();
+import pkg from '@prisma/client';
 
-const { Pool } = pg;
-export const connection = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+const { PrismaClient } = pkg;
 
+export const prisma = new PrismaClient();
 
 
