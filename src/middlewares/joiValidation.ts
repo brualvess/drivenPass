6a48b9fd33da4b00fbc86decb4c6cc2a59a5c81d
brualvess <oliveirabrunna2210@gmail.com>
2022-09-10 10:54:35 -0300

    fix

--- src/middlewares/joiValidation.ts
+++ src/middlewares/joiValidation.ts
@@ -2,7 +2,6 @@ import joi from 'joi'
 import { Request, Response, NextFunction  } from "express";
 const joiValidation = (schema:joi.ObjectSchema)=>{
 	return (req : Request, res : Response, next:NextFunction)=>{
-		console.log((req.body.userId))
 		const validation = schema.validate(req.body);
 		if (validation.error) {
 			return res.status(422).send(validation.error.details.map(detail => detail.message));
